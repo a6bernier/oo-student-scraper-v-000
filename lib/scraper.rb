6cc3742e16084351a824d1da3c5097d397d26999
Andrew Bernier <a6bernier@gmail.com>
2018-10-31 16:14:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,22 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+      def self.scrape_index_page(index_url)
+        doc = Nokogiri::HTML(open(index_url))
+        students = []
+        doc.css("div.student-card").each do |student|
+          students << {
+            name: student.css("h4").text,
+            location: student.css("p").text,
+            profile_url: student.css("a").attribute("href").value
+          }
+        end
+        students
+      end
+
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
